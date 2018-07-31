9fb83f31d5c8c074f6749377197ae66909e216c0
cms_huyuhui <243818817@qq.com>
2018-07-31 09:13:48 +0800

    新增使用rdma文件传输的例子，使用libevent多路复用ibv_comp_channel->fd

---
+++ 04_mlnx_rdmacm_file_transfer/client_to_proxy.c
@@ -0,0 +1,509 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <uuid/uuid.h>
+
+#include <rdma/rdma_cma.h>
+
+
+const char *DEFAULT_PORT = "12345";
+const size_t BUFFER_SIZE = 10 * 1024 * 1024;
+const int TIMEOUT_IN_MS = 500;
+
+enum message_id
+{
+    MSG_INVALID = 0,
+    MSG_MR,// 同步mr数据
+    MSG_READY,//同步告知client,server已可接收数据
+    MSG_DONE//同步告知client,server 准备断开
+};
+
+struct message
+{
+    int id;
+
+    // 交流rkey之用
+    union
+    {
+        struct
+        {
+            uint64_t addr;
+            uint32_t rkey;
+        } mr;
+    } data;
+};
+
+
+
+struct client_context
+{
+    char *buffer;//send_buffer
+    struct ibv_mr *buffer_mr;
+
+    struct message *msg;
+    struct ibv_mr *msg_mr;
+
+    char *recv_buffer;//recv_buffer
+    struct ibv_mr *recv_buffer_mr;
+
+
+    uint64_t peer_addr;
+    uint32_t peer_rkey;
+
+    int fd;
+    const char *file_name;
+};
+
+struct context 
+{
+    struct ibv_context *ctx;
+    struct ibv_pd *pd;
+    struct ibv_cq *cq;
+    struct ibv_comp_channel *comp_channel;
+
+    pthread_t cq_poller_thread;
+};
+static struct context *s_ctx = NULL;
+
+static void on_completion(struct ibv_wc *wc);
+static void on_pre_conn(struct rdma_cm_id *id);
+
+
+void rc_disconnect(struct rdma_cm_id *id)
+{
+    rdma_disconnect(id);
+}
+
+void rc_die(const char *reason)
+{
+    fprintf(stderr, "%s\n", reason);
+    exit(EXIT_FAILURE);
+}
+
+struct ibv_pd* rc_get_pd()
+{
+    return s_ctx->pd;
+}
+
+
+void * poll_cq(void *ctx)
+{
+    struct ibv_cq *cq;
+    struct ibv_wc wc;
+    int ret = 0 ;
+    while (1) 
+    {
+        ret = ibv_get_cq_event(s_ctx->comp_channel, &cq, &ctx);
+        if (0 != ret )
+        {
+            fprintf(stderr,"poll_cq ibv_get_cq_event error: %s\n",strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            // fprintf(stderr,"poll_cq ibv_get_cq_event success\n");
+
+        }
+
+        ibv_ack_cq_events(cq, 1);
+        ret = ibv_req_notify_cq(cq, 0);
+        if (0 != ret)
+        {
+            fprintf(stderr,"poll_cq ibv_req_notify_cq error: %s\n",strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        while (ibv_poll_cq(cq, 1, &wc)) 
+        {
+            if (wc.status == IBV_WC_SUCCESS)
+            {
+                on_completion(&wc);
+            }
+            else
+            {
+                rc_die("poll_cq: status is not IBV_WC_SUCCESS");
+            }
+        }
+    }
+
+    return NULL;
+}
+
+
+void build_context(struct ibv_context *verbs)
+{
+    if (s_ctx) 
+    {   
+        if (s_ctx->ctx != verbs)
+        {
+            rc_die("cannot handle events in more than one context.");
+        }
+        return;
+    }
+
+    s_ctx = (struct context *)malloc(sizeof(struct context));
+
+    s_ctx->ctx = verbs;
+
+    s_ctx->pd = ibv_alloc_pd(s_ctx->ctx);
+    if (NULL == s_ctx->pd)
+    {
+        rc_die("ibv_alloc_pd error\n");
+    }
+    
+    s_ctx->comp_channel = ibv_create_comp_channel(s_ctx->ctx);
+    if ( NULL == s_ctx->comp_channel )
+    {
+        rc_die("ibv_create_comp_channel error");
+    }
+    
+    s_ctx->cq = ibv_create_cq(s_ctx->ctx, 10, NULL, s_ctx->comp_channel, 0); /* cqe=10 is arbitrary */
+    if ( NULL == s_ctx->cq )
+    {
+        rc_die("ibv_create_cq error");
+    }
+
+    int ret = 0;
+    ret = ibv_req_notify_cq(s_ctx->cq, 0);
+    if (0 != ret)
+    {
+        fprintf(stderr,"ibv_req_notify_cq error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ret = pthread_create(&s_ctx->cq_poller_thread, NULL, poll_cq, NULL);
+    if (0 != ret )
+    {
+        rc_die("can not pthread_create to poll_cq");
+    }
+
+}
+
+void build_params(struct rdma_conn_param *params)
+{
+    memset(params, 0, sizeof(*params));
+
+    params->initiator_depth = 1;
+    params->responder_resources = 1;
+    params->rnr_retry_count = 7; /* infinite retry */
+}
+
+void build_qp_attr(struct ibv_qp_init_attr *qp_attr)
+{
+    memset(qp_attr, 0, sizeof(*qp_attr));
+
+    qp_attr->send_cq = s_ctx->cq;
+    qp_attr->recv_cq = s_ctx->cq;
+    qp_attr->qp_type = IBV_QPT_RC;
+
+    qp_attr->cap.max_send_wr = 10;
+    qp_attr->cap.max_recv_wr = 10;
+    qp_attr->cap.max_send_sge = 1;
+    qp_attr->cap.max_recv_sge = 1;
+}
+
+void build_connection(struct rdma_cm_id *id)
+{
+    struct ibv_qp_init_attr qp_attr;
+
+    build_context(id->verbs);
+    build_qp_attr(&qp_attr);
+
+    int ret = 0;
+    ret = rdma_create_qp(id, s_ctx->pd, &qp_attr);
+    if (0 != ret )
+    {
+        fprintf(stderr,"rdma_create_qp error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void send_next_chunk(struct rdma_cm_id *id);
+static void post_receive(struct rdma_cm_id *id);
+
+
+void event_loop(struct rdma_event_channel *ec)
+{
+    struct rdma_cm_event *event = NULL;
+    struct rdma_conn_param cm_params;
+    int ret = 0;
+
+    build_params(&cm_params);
+
+    while (rdma_get_cm_event(ec, &event) == 0) 
+    {
+        struct rdma_cm_event event_copy;
+
+        memcpy(&event_copy, event, sizeof(*event));
+        rdma_ack_cm_event(event);
+
+        if (event_copy.event == RDMA_CM_EVENT_ADDR_RESOLVED) // client
+        {
+            printf("rdma_get_cm_event() == RDMA_CM_EVENT_ADDR_RESOLVED\n");
+
+            build_connection(event_copy.id);
+
+            on_pre_conn(event_copy.id);
+
+            ret = rdma_resolve_route(event_copy.id, TIMEOUT_IN_MS);
+            if (0 != ret )
+            {
+                fprintf(stderr,"rdma_resolve_route error: %s\n",strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        } 
+        else if (event_copy.event == RDMA_CM_EVENT_ROUTE_RESOLVED) // client
+        {
+            printf("rdma_get_cm_event() == RDMA_CM_EVENT_ROUTE_RESOLVED\n");
+
+            ret = rdma_connect(event_copy.id, &cm_params);
+            if (0 != ret )
+            {
+                fprintf(stderr,"rdma_connect error: %s\n",strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        } 
+        else if (event_copy.event == RDMA_CM_EVENT_ESTABLISHED) 
+        {
+            printf("rdma_get_cm_event() == RDMA_CM_EVENT_ESTABLISHED\n");
+            post_receive(event_copy.id);
+            send_next_chunk(event_copy.id);
+
+        } 
+        else if (event_copy.event == RDMA_CM_EVENT_DISCONNECTED) 
+        {
+            rdma_destroy_qp(event_copy.id);
+
+            // rc_disconnect(event_copy.id);
+
+            rdma_destroy_id(event_copy.id);
+            break;
+
+        } 
+        else 
+        {
+            rc_die("rdma_get_cm_event() got a unknown event\n");
+        }
+    }
+}
+
+
+void rc_client_loop(const char *host, const char *port, void *context)
+{
+    struct addrinfo *addr;
+    struct rdma_cm_id *conn = NULL;
+    struct rdma_event_channel *ec = NULL;
+    struct rdma_conn_param cm_params;
+
+    int ret = 0;
+
+    ret = getaddrinfo(host, port, NULL, &addr);
+    if (0 != ret )
+    {
+        fprintf(stderr,"getaddrinfo error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ec = rdma_create_event_channel();
+    if (NULL == ec)
+    {
+        fprintf(stderr,"rdma_create_event_channel error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ret = rdma_create_id(ec, &conn, NULL, RDMA_PS_TCP);
+    if (0 != ret)
+    {
+        fprintf(stderr,"rdma_create_id error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ret = rdma_resolve_addr(conn, NULL, addr->ai_addr, TIMEOUT_IN_MS);
+    if (0 != ret )
+    {
+        fprintf(stderr,"rdma_resolve_addr error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(addr);
+
+    conn->context = context;
+
+    build_params(&cm_params);
+
+    event_loop(ec); // exit on disconnect
+
+    rdma_destroy_event_channel(ec);
+}
+
+
+
+
+
+/*****************************************
+* Function: post_send
+*****************************************/
+static void write_remote(struct rdma_cm_id *id, uint32_t len)
+{
+    struct client_context *ctx = (struct client_context *)id->context;
+
+    struct ibv_send_wr sr;
+    struct ibv_sge sge;
+    struct ibv_send_wr *bad_wr;
+    int rc;
+
+    /* prepare the scatter/gather entry */
+    memset(&sge, 0, sizeof(sge));
+
+    sge.addr = (uintptr_t)ctx->buffer;
+    sge.length = len;
+    sge.lkey = ctx->buffer_mr->lkey;
+
+    /* prepare the SR */
+    memset(&sr, 0, sizeof(sr));
+
+    sr.next       = NULL;
+    sr.wr_id      = (uintptr_t)id;
+    sr.sg_list    = &sge;
+    sr.num_sge    = 1;
+    sr.opcode     = IBV_WR_SEND_WITH_IMM;
+    sr.send_flags = IBV_SEND_SIGNALED;
+    sr.imm_data = htonl(len);
+
+    printf("start send uuid: %s\n",ctx->buffer );
+
+    /* there is a Receive Request in the responder side, so we won't get any into RNR flow */
+    rc = ibv_post_send(id->qp, &sr, &bad_wr);
+    if (rc) 
+    {
+        fprintf(stderr, "failed to post SR\n");
+    }
+}
+
+/*****************************************
+* Function: post_receive
+*****************************************/
+
+static void post_receive(struct rdma_cm_id *id)
+{
+    struct client_context *ctx = (struct client_context *)id->context;
+
+    struct ibv_recv_wr wr, *bad_wr = NULL;
+    struct ibv_sge sge;
+
+    memset(&wr, 0, sizeof(wr));
+
+    wr.wr_id = (uintptr_t)id;
+    wr.sg_list = &sge;
+    wr.num_sge = 1;
+
+    sge.addr = (uintptr_t)ctx->recv_buffer;
+    sge.length = BUFFER_SIZE;
+    sge.lkey = ctx->recv_buffer_mr->lkey;
+
+     int ret = ibv_post_recv(id->qp, &wr, &bad_wr);
+     if ( 0 != ret )
+     {
+         fprintf(stderr,"ibv_post_recv error : %s\n",strerror(errno));
+         exit(EXIT_FAILURE);
+     }
+}
+
+
+
+
+static void send_next_chunk(struct rdma_cm_id *id)
+{
+    uuid_t uuid;
+    char str[256];
+    memset(str,0,256);
+    uuid_generate(uuid);
+    uuid_unparse(uuid, str);
+
+    struct client_context *ctx = (struct client_context *)id->context;
+    ssize_t size = 0;
+    strcpy(ctx->buffer, str);
+    size = strlen(ctx->buffer);
+    write_remote(id,size);
+}
+
+static void on_pre_conn(struct rdma_cm_id *id)
+{
+    struct client_context *ctx = (struct client_context *)id->context;
+
+    posix_memalign((void **)&ctx->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
+    ctx->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE);
+    if (NULL == ctx->buffer_mr)
+    {
+        rc_die("ctx->buffer_mr ibv_reg_mr() error\n");
+    }
+
+    posix_memalign((void **)&ctx->recv_buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
+    ctx->recv_buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->recv_buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE);
+    if (NULL == ctx->recv_buffer_mr)
+    {
+        rc_die("ctx->buffer_mr ibv_reg_mr() error\n");
+    }
+
+
+    posix_memalign((void **)&ctx->msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->msg));
+    ctx->msg_mr = ibv_reg_mr(rc_get_pd(), ctx->msg, sizeof(*ctx->msg), IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE);
+    if (NULL == ctx->msg_mr)
+    {
+        rc_die("ctx->msg_mr ibv_reg_mr() error\n");
+    }
+    
+    post_receive(id);
+
+}
+
+static void on_completion(struct ibv_wc *wc)
+{
+    struct rdma_cm_id *id = (struct rdma_cm_id *)(uintptr_t)(wc->wr_id);
+    struct client_context *ctx = (struct client_context *)id->context;
+
+    if (wc->opcode == IBV_WC_RECV) 
+    {
+        // printf("ibv_post_recv() poll_cq() success\n");
+        sleep(3);
+        
+        printf("received uuid  %s\n\n\n",ctx->recv_buffer);
+        post_receive(id);
+        send_next_chunk(id);
+    }
+    else if( wc->opcode == IBV_WC_SEND)
+    {
+        // printf("ibv_post_send() poll_cq() success  %s\n",ctx->recv_buffer);
+
+    }
+    else
+    {
+        // printf("on_completion other event wc->opcode : %d\n",wc->opcode);
+    }
+}
+
+// ./client 192.168.40.4 test
+int main(int argc, char **argv)
+{
+    struct client_context ctx;
+
+    if (argc != 3) 
+    {
+        fprintf(stderr, "usage: %s <server-address> <server-port>\n", argv[0]);
+        return 1;
+    }
+
+    // ctx.file_name = basename(argv[2]);
+    // ctx.fd = open(argv[2], O_RDONLY);
+    DEFAULT_PORT = argv[2];
+
+    rc_client_loop(argv[1], DEFAULT_PORT, &ctx);
+
+    // close(ctx.fd);
+
+    return 0;
+}
---
+++ 04_mlnx_rdmacm_file_transfer/server.c
@@ -0,0 +1,493 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <rdma/rdma_cma.h>
+#define MAX_FILE_NAME 256
+const int TIMEOUT_IN_MS = 500;
+
+struct context 
+{
+    struct ibv_context *ctx;
+    struct ibv_pd *pd;
+    struct ibv_cq *cq;
+    struct ibv_comp_channel *comp_channel;
+
+    pthread_t cq_poller_thread;
+};
+
+static struct context *s_ctx = NULL;
+
+const char *DEFAULT_PORT = "12345";
+const size_t BUFFER_SIZE = 10 * 1024 * 1024;
+
+enum message_id
+{
+    MSG_INVALID = 0,
+    MSG_MR,// 同步mr数据
+    MSG_READY,//同步告知client,server已可接收数据
+    MSG_DONE//同步告知client,server 准备断开
+};
+
+struct message
+{
+    int id;
+
+    // 交流rkey之用
+    union
+    {
+        struct
+        {
+            uint64_t addr;
+            uint32_t rkey;
+        } mr;
+    } data;
+};
+
+
+struct conn_context
+{
+    char *buffer;
+    struct ibv_mr *buffer_mr;
+
+    // message 留给 rdma read/write 交流使用
+    struct message *msg;
+    struct ibv_mr *msg_mr;
+
+    int fd;
+    char file_name[MAX_FILE_NAME];
+};
+
+
+void rc_disconnect(struct rdma_cm_id *id)
+{
+    rdma_disconnect(id);
+}
+
+void rc_die(const char *reason)
+{
+    fprintf(stderr, "%s\n", reason);
+    exit(EXIT_FAILURE);
+}
+
+struct ibv_pd* rc_get_pd()
+{
+    return s_ctx->pd;
+}
+
+static void on_completion(struct ibv_wc *wc);
+
+void * poll_cq(void *ctx)
+{
+    struct ibv_cq *cq;
+    struct ibv_wc wc;
+    int ret = 0 ;
+    while (1) 
+    {
+        ret = ibv_get_cq_event(s_ctx->comp_channel, &cq, &ctx);
+        if (0 != ret )
+        {
+            fprintf(stderr,"poll_cq ibv_get_cq_event error: %s\n",strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        ibv_ack_cq_events(cq, 1);
+
+        ret = ibv_req_notify_cq(cq, 0);
+        if (0 != ret)
+        {
+            fprintf(stderr,"poll_cq ibv_req_notify_cq error: %s\n",strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        while (ibv_poll_cq(cq, 1, &wc)) 
+        {
+            if (wc.status == IBV_WC_SUCCESS)
+            {
+                on_completion(&wc);
+            }
+            else
+            {
+                rc_die("poll_cq: status is not IBV_WC_SUCCESS");
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void build_context(struct ibv_context *verbs)
+{
+    if (s_ctx) 
+    {   
+        if (s_ctx->ctx != verbs)
+        {
+            rc_die("cannot handle events in more than one context.");
+        }
+        return;
+    }
+
+    s_ctx = (struct context *)malloc(sizeof(struct context));
+
+    s_ctx->ctx = verbs;
+
+    s_ctx->pd = ibv_alloc_pd(s_ctx->ctx);
+    if (NULL == s_ctx->pd)
+    {
+        fprintf(stderr,"ibv_alloc_pd error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    s_ctx->comp_channel = ibv_create_comp_channel(s_ctx->ctx);
+    if (NULL == s_ctx->comp_channel )
+    {
+        fprintf(stderr,"ibv_create_comp_channel error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    s_ctx->cq = ibv_create_cq(s_ctx->ctx, 10, NULL, s_ctx->comp_channel, 0); /* cqe=10 is arbitrary */
+    if (NULL == s_ctx->cq)
+    {
+        fprintf(stderr,"ibv_create_cq error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    int ret = 0;
+    ret = ibv_req_notify_cq(s_ctx->cq, 0);
+    if (0 != ret)
+    {
+        fprintf(stderr,"ibv_req_notify_cq error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ret = pthread_create(&s_ctx->cq_poller_thread, NULL, poll_cq, NULL);
+    if (0 != ret)
+    {
+        rc_die("cannot pthread_create to poll_cq\n");
+    }
+}
+
+void build_params(struct rdma_conn_param *params)
+{
+    memset(params, 0, sizeof(*params));
+
+    params->initiator_depth = 1;
+    params->responder_resources = 1;
+    params->rnr_retry_count = 7; /* infinite retry */
+}
+
+void build_qp_attr(struct ibv_qp_init_attr *qp_attr)
+{
+    memset(qp_attr, 0, sizeof(*qp_attr));
+
+    qp_attr->send_cq = s_ctx->cq;
+    qp_attr->recv_cq = s_ctx->cq;
+    qp_attr->qp_type = IBV_QPT_RC;
+
+    qp_attr->cap.max_send_wr = 10;
+    qp_attr->cap.max_recv_wr = 10;
+    qp_attr->cap.max_send_sge = 1;
+    qp_attr->cap.max_recv_sge = 1;
+}
+
+void build_connection(struct rdma_cm_id *id)
+{
+    struct ibv_qp_init_attr qp_attr;
+
+    fprintf(stderr, "id->verbs = %p\n",id->verbs );// always same
+    build_context(id->verbs);
+    build_qp_attr(&qp_attr);
+
+    int ret = 0; 
+    ret = rdma_create_qp(id, s_ctx->pd, &qp_attr);
+    if (0 != ret )
+    {
+        fprintf(stderr,"rdma_create_qp error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+static void send_message(struct rdma_cm_id *id)
+{
+    struct conn_context *ctx = (struct conn_context *)id->context;
+
+    struct ibv_send_wr wr, *bad_wr = NULL;
+    struct ibv_sge sge;
+
+    memset(&wr, 0, sizeof(wr));
+
+    wr.wr_id = (uintptr_t)id;
+    wr.opcode = IBV_WR_SEND;
+    wr.sg_list = &sge;
+    wr.num_sge = 1;
+    wr.send_flags = IBV_SEND_SIGNALED;
+
+    sge.addr = (uintptr_t)ctx->msg;
+    sge.length = sizeof(*ctx->msg);
+    sge.lkey = ctx->msg_mr->lkey;
+
+    int ret = 0 ;
+    ibv_post_send(id->qp, &wr, &bad_wr);
+    if (0 != ret )
+    {
+        fprintf(stderr,"ibv_post_send error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void post_receive(struct rdma_cm_id *id)
+{
+    struct ibv_recv_wr wr, *bad_wr = NULL;
+
+    memset(&wr, 0, sizeof(wr));
+
+    wr.wr_id = (uintptr_t)id;
+    wr.sg_list = NULL;
+    wr.num_sge = 0;
+
+    int ret = 0 ;
+    ret = ibv_post_recv(id->qp, &wr, &bad_wr);
+    if (0 != ret )
+    {
+        fprintf(stderr,"ibv_post_recv error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void on_pre_conn(struct rdma_cm_id *id)
+{
+    struct conn_context *ctx = (struct conn_context *)malloc(sizeof(struct conn_context));
+
+    id->context = ctx;
+
+    ctx->file_name[0] = '\0'; // take this to mean we don't have the file name
+
+    posix_memalign((void **)&ctx->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
+    ctx->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE);
+    if ( NULL == ctx->buffer_mr )
+    {
+        rc_die("ctx->buffer_mr ibv_reg_mr() error\n");
+    }
+
+    posix_memalign((void **)&ctx->msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->msg));
+    ctx->msg_mr = ibv_reg_mr(rc_get_pd(), ctx->msg, sizeof(*ctx->msg), IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE);
+    if (NULL == ctx->msg_mr)
+    {
+        rc_die("ctx->msg_mr ibv_reg_mr() error");
+    }
+    post_receive(id);
+}
+
+static void on_connection(struct rdma_cm_id *id)
+{
+    struct conn_context *ctx = (struct conn_context *)id->context;
+
+    ctx->msg->id = MSG_MR;
+    ctx->msg->data.mr.addr = (uintptr_t)ctx->buffer_mr->addr;
+    ctx->msg->data.mr.rkey = ctx->buffer_mr->rkey;
+
+    send_message(id);
+}
+
+static void on_completion(struct ibv_wc *wc)
+{
+    struct rdma_cm_id *id = (struct rdma_cm_id *)(uintptr_t)wc->wr_id;
+    struct conn_context *ctx = (struct conn_context *)id->context;
+
+    if (wc->opcode == IBV_WC_RECV_RDMA_WITH_IMM) 
+    {
+        uint32_t size = ntohl(wc->imm_data);
+
+        if (size == 0) 
+        {
+            ctx->msg->id = MSG_DONE;
+            send_message(id);
+            // don't need post_receive() since we're done with this connection
+        } 
+        else if (ctx->file_name[0]) 
+        {
+            ssize_t ret;
+
+            printf(" %lu received %i bytes.\n", pthread_self(),size);
+
+            ret = write(ctx->fd, ctx->buffer, size);
+
+            if (ret != size)
+            {
+                rc_die("write() failed");
+            }
+
+            post_receive(id);
+
+            ctx->msg->id = MSG_READY;
+            send_message(id);
+
+        } 
+        else 
+        {
+            memcpy(ctx->file_name, ctx->buffer, (size > MAX_FILE_NAME) ? MAX_FILE_NAME : size);
+            ctx->file_name[size - 1] = '\0';
+
+            printf("opening file %s\n", ctx->file_name);
+
+            ctx->fd = open(ctx->file_name, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+            if (ctx->fd == -1)
+            {
+                rc_die("open() failed");
+            }
+
+            post_receive(id);
+
+            ctx->msg->id = MSG_READY;
+            send_message(id);
+        }
+    }
+}
+
+static void on_disconnect(struct rdma_cm_id *id)
+{
+    struct conn_context *ctx = (struct conn_context *)id->context;
+
+    close(ctx->fd);
+
+    ibv_dereg_mr(ctx->buffer_mr);
+    ibv_dereg_mr(ctx->msg_mr);
+
+    free(ctx->buffer);
+    free(ctx->msg);
+
+    printf("finished transferring %s\n", ctx->file_name);
+
+    free(ctx);
+}
+
+
+void event_loop(struct rdma_event_channel *ec)
+{
+    struct rdma_cm_event *event = NULL;
+    struct rdma_conn_param cm_params;
+
+    build_params(&cm_params);
+
+    int ret = 0;
+
+    while (rdma_get_cm_event(ec, &event) == 0) 
+    {
+        struct rdma_cm_event event_copy;
+
+        memcpy(&event_copy, event, sizeof(*event));
+        rdma_ack_cm_event(event);
+
+        if (event_copy.event == RDMA_CM_EVENT_CONNECT_REQUEST) // server
+        {
+            fprintf(stderr,"event_copy.id: %p\n",event_copy.id);// change every time
+
+            build_connection(event_copy.id);
+
+            on_pre_conn(event_copy.id);
+
+            ret = rdma_accept(event_copy.id, &cm_params);
+            if (0 != ret )
+            {
+                fprintf(stderr,"event loog rdma_accept() error: %s\n",strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+
+        } 
+        else if (event_copy.event == RDMA_CM_EVENT_ESTABLISHED) 
+        {
+            on_connection(event_copy.id);
+        } 
+        else if (event_copy.event == RDMA_CM_EVENT_DISCONNECTED) 
+        {
+            rdma_destroy_qp(event_copy.id);
+
+            on_disconnect(event_copy.id);
+
+            rdma_destroy_id(event_copy.id);
+
+        } 
+        else 
+        {
+            rc_die("unknown event\n");
+        }
+    }
+}
+
+void rc_server_loop(const char *port)
+{
+    struct sockaddr_in6 addr;
+    struct rdma_cm_id *listener = NULL;
+    struct rdma_event_channel *ec = NULL;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin6_family = AF_INET6;
+    addr.sin6_port = htons(atoi(port));
+
+    ec = rdma_create_event_channel();
+    if (NULL == ec)
+    {
+        fprintf(stderr,"rdma_create_event_channel error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        fprintf(stderr, "%s\n","rdma_create_event_channel success" );
+    }
+
+    int ret = 0;
+    ret = rdma_create_id(ec, &listener, NULL, RDMA_PS_TCP);
+    if (0 != ret )
+    {
+        fprintf(stderr,"rdma_create_id error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        fprintf(stderr, "%s\n","rdma_create_id success" );
+    }
+
+
+    ret = rdma_bind_addr(listener, (struct sockaddr *)&addr);
+    if (0 != ret)
+    {
+        fprintf(stderr,"rdma_bind_addr error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        fprintf(stderr, "%s\n","rdma_bind_addr success" );
+    }
+
+    ret = rdma_listen(listener, 10); /* backlog=10 is arbitrary */
+    if (0 != ret )
+    {
+        fprintf(stderr,"rdma_listen error: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        fprintf(stderr, "%s\n","rdma_listen success" );
+    }
+
+
+    event_loop(ec);
+
+    rdma_destroy_id(listener);
+    rdma_destroy_event_channel(ec);
+}
+
+
+
+
+// ./server
+int main(int argc, char **argv)
+{
+    printf("waiting for connections. interrupt (^C) to exit.\n");
+
+    rc_server_loop(DEFAULT_PORT);
+
+    return 0;
+}
